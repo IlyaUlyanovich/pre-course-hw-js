e4b42301dfc578d1cd161d1cdb1e918dffb25356
Ilya Ulyanovich <ilyaulyanovich@gmail.com>
2024-01-17 22:03:22 +0300

    HW Task 6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -12,4 +12,4 @@ const newPassport = JSON.parse(JSON.stringify(passportWithAddress));
 newPassport.address.city = "Bobryisk";
 
 console.log(passportWithAddress.address.city);  
-console.log(newPassport.address.city);
+console.log(newPassport.address.city); 
