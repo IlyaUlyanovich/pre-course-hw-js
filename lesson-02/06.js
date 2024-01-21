bebf7f0e09e3e8f62622b1efc51258f5b7eb500f
Ilya Ulyanovich <ilyaulyanovich@gmail.com>
2024-01-21 20:50:51 +0300

    HW Task 6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -7,11 +7,9 @@ let passportWithAddress = {
     }
 };
 
-// Create a deep copy of the object using JSON.parse() and JSON.stringify()
 let clonedPassport = JSON.parse(JSON.stringify(passportWithAddress));
 
-// Modify the cloned object
 clonedPassport.address.city = 'Bobryisk';
 
-console.log(passportWithAddress.address.city);  // Output: "LA"
-console.log(clonedPassport.address.city);  // Output: "Bobryisk"
+console.log(passportWithAddress.address.city); 
+console.log(clonedPassport.address.city);  
