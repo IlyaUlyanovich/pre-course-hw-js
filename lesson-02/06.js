6d640b0be3c9b3240d4bcea8a94517314ab84bb7
Ilya Ulyanovich <ilyaulyanovich@gmail.com>
2024-01-21 19:47:20 +0300

    HW Task 6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,17 +1,18 @@
-let passportWithAddress = {
-    name: "Petr",
-    surname: "Petrov",
-    address: {
-        country: "USA",
-        city: "LA"
-    }
-};
-
-// Create a deep copy of the object using the spread operator and object destructuring
-let clonedPassport = { ...passportWithAddress, address: { ...passportWithAddress.address } };
-
-// Modify the cloned object
-clonedPassport.address.city = 'Bobryisk';
-
-console.log(passportWithAddress.address.city);  // Output: "LA"
-console.log(clonedPassport.address.city);  // Output: "Bobryisk"
+function deepClone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+  }
+  
+  let passportWithAddress = {
+      name: "Petr",
+      surname: "Petrov",
+      address: {
+          country: "USA",
+          city: "LA"
+      }
+  };
+  
+  let clonedPassport = deepClone(passportWithAddress);
+  clonedPassport.address.city = 'Bobryisk';
+  
+  console.log(passportWithAddress.address.city);  // Output: "LA"
+  console.log(clonedPassport.address.city);  // Output: "Bobryisk"
