722262a0a96694b63ed98b580f6d39b1a5fb33d0
Ilya Ulyanovich <ilyaulyanovich@gmail.com>
2024-01-21 20:15:04 +0300

    HW Task 6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,18 +1,15 @@
-function deepClone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-  }
-  
-  let passportWithAddress = {
-      name: "Petr",
-      surname: "Petrov",
-      address: {
-          country: "USA",
-          city: "LA"
-      }
-  };
-  
-  let clonedPassport = deepClone(passportWithAddress);
-  clonedPassport.address.city = 'Bobryisk';
-  
-  console.log(passportWithAddress.address.city);  // Output: "LA"
-  console.log(clonedPassport.address.city);  // Output: "Bobryisk"
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+
+let clonedPassport = structuredClone(passportWithAddress);
+clonedPassport.address.city = 'Bobryisk';
+
+console.log(passportWithAddress.address.city);  
+console.log(clonedPassport.address.city);  
